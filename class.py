92f73010ab5ea96b992b8fa34e3cdd3a2452e8e3
Florence Mnyazi <florencemnyazi@gmail.com>
2017-03-07 16:47:56 +0300

    inheritance in OOP

--- class.py
+++ class.py
@@ -2,3 +2,9 @@ class Person:
 	def _init_(self, name,surname):
 		self.name = name
 		self.surname = surname
+
+class staff(Person):
+	Entrylevel, Midlevel, Seniorlevel = range(2)
+	def _init_(self,employment_level,):
+		self.employment_level = employment_level
+ 
