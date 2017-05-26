d5921693594953f62c2b2a242c406808edfb0565
Flo Mnyazi <florencemnyazi@gmail.com>
2017-05-26 12:02:22 +0300

    Update class.py

--- class.py
+++ class.py
@@ -1,10 +1,2 @@
-class Person:
-	def _init_(self, name,surname):
-		self.name = name
-		self.surname = surname
 
-class staff(Person):
-	Entrylevel, Midlevel, Seniorlevel = range(2)
-	def _init_(self,employment_level,):
-		self.employment_level = employment_level
  
